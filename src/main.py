be5c61c5a33f85cb65cc2dffeace503085bb3531
Fredrik vW <fredrik@waldnet.se>
2017-01-02 20:33:29 +0100

    better blinking

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 import explorerhat
 import time, threading ## Import 'time' library. Allows us to use 'sleep'
 
+blinker = 0
 @app.route('/')
 def hello_world():
     start_blink()
     return 'Hello to the World!'
 
 def start_blink():
+    blinker++
     threading.Timer(1, blink).start()
 
 def blink():
@@ -18,8 +20,10 @@ def blink():
     threading.Timer(5, stop_blink).start()
 
 def stop_blink():
-    print "stop_blink"
-    explorerhat.output[0].stop()
+    print "stop_blink" + blinker
+    if blinker == 1:
+        explorerhat.output[0].stop()
+    blinker--
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
