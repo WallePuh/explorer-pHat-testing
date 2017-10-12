1c9e16160128a4d056f20072bf9c3ddbffb69cc1
Fredrik vW <fredrik@waldnet.se>
2017-10-12 14:16:36 +0200

    just blink

--- src/main.py
+++ src/main.py
@@ -7,23 +7,24 @@ import time, threading ## Import 'time' library. Allows us to use 'sleep'
 blinker = 0
 @app.route('/')
 def hello_world():
-    start_blink()
-    return 'Hello to the World!'
+    blink()
+    return '<h1>Hello Villemo!</h1>'
 
-def start_blink():
-    blinker+= 1
-    threading.Timer(1, blink).start()
+# def start_blink():
+#     blinker+= 1
+#     threading.Timer(1, blink).start()
 
 def blink():
     print "blink"
     explorerhat.output[0].blink()
-    threading.Timer(5, stop_blink).start()
+    explorerhat.output[1].blink()
+    # threading.Timer(5, stop_blink).start()
 
-def stop_blink():
-    print "stop_blink" + blinker
-    if blinker == 1:
-        explorerhat.output[0].stop()
-    blinker-= 1
+# def stop_blink():
+#     print "stop_blink" + blinker
+#     if blinker == 1:
+#         explorerhat.output[0].stop()
+#     blinker-= 1
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
